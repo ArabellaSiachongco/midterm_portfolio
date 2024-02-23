064b7eaabe4508dfe27a1d6d0d609214ac1c69c3
ArabellaSiachongco <aps9434@students.uc-bcf.edu.ph>
2024-02-23 11:33:00 +0800

    to add in a repository

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+// this needds to be imported for the hamburger to work 
